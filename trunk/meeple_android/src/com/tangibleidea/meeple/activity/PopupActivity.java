9a7c3f9c062940b0a9acc1a080e1a1fc1607985a
thks10 <thks10@cb0aef14-ba3f-4046-a42c-e7904d01ad88>
2012-04-11 17:22:27 +0000

    -팝업창 작업 -최근대화탭 개선 -사진 배경색 개선 -리소스 추가.

--- trunk/meeple_android/src/com/tangibleidea/meeple/activity/PopupActivity.java
+++ trunk/meeple_android/src/com/tangibleidea/meeple/activity/PopupActivity.java
@@ -1,9 +1,13 @@
 package com.tangibleidea.meeple.activity;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.graphics.Rect;
 import android.os.Bundle;
 import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
@@ -11,10 +15,11 @@ import android.widget.TextView;
 import com.tangibleidea.meeple.R;
 import com.tangibleidea.meeple.server.RequestImageMethods;
 
-public class PopupActivity extends Activity
+public class PopupActivity extends Activity implements OnClickListener
 {
 	RelativeLayout RL_window;
 	ImageView IMG_profile;
+	Button BTN_close;
 	TextView TXT_name, TXT_profile, TXT_info;
 
 	/* (non-Javadoc)
@@ -31,6 +36,9 @@ public class PopupActivity extends Activity
 		TXT_profile= (TextView) findViewById(R.id.txt_popup_profile);
 		TXT_info= (TextView) findViewById(R.id.txt_popup_info);
 		IMG_profile= (ImageView) findViewById(R.id.img_popup_photo);
+		BTN_close= (Button) findViewById(R.id.btn_popup_exit);
+		BTN_close.setOnClickListener(this);
+		
 		RL_window= (RelativeLayout) findViewById(R.id.view_popupwindow_all);
 		
 		TXT_name.setText( this.getIntent().getStringExtra("name") );
@@ -38,7 +46,8 @@ public class PopupActivity extends Activity
 		//TXT_name.setText( this.getIntent().getStringExtra("info") );
 		
 		RequestImageMethods RIM= new RequestImageMethods();
-		RIM.DownloadImage2( IMG_profile, this.getIntent().getStringExtra("name") );	// 이미지를 다운로드 받고    	
+		RIM.DownloadImage2( IMG_profile, this.getIntent().getStringExtra("id") );	// 이미지를 다운로드 받고
+		IMG_profile.setBackgroundColor(Color.BLACK);
 	}
 
 	/* (non-Javadoc)
@@ -57,6 +66,16 @@ public class PopupActivity extends Activity
 		
 		
 		return super.onTouchEvent(event);
+	}
+
+	@Override
+	public void onClick(View v)
+	{
+		if(v.getId() == R.id.btn_popup_exit)
+		{
+			finish();
+		}
+		
 	}
 	
 }
--- trunk/meeple_android/src/com/tangibleidea/meeple/layout/RecentTalkListAdapter.java
+++ trunk/meeple_android/src/com/tangibleidea/meeple/layout/RecentTalkListAdapter.java
@@ -11,8 +11,10 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewGroup.LayoutParams;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.ImageView.ScaleType;
 import android.widget.TextView;
 
 import com.tangibleidea.meeple.R;
@@ -22,15 +24,14 @@ public class RecentTalkListAdapter extends ArrayAdapter<RecentTalkEntry>
 {
 	private ArrayList<RecentTalkEntry> items;
 	private int viewResource;
-	private Context context;
-	private final static int s_SEPRATE_POS= 0;	// 구분자가 들어가는 위치
+	private Context mContext;
 	
-	private HashMap<Integer,Boolean> mapRecenttalkLabel= new HashMap<Integer,Boolean>();
+	//private HashMap<Integer,Boolean> mapRecenttalkLabel= new HashMap<Integer,Boolean>();
 	  
 	public RecentTalkListAdapter(Context context, int rsrcId, int txtId, ArrayList<RecentTalkEntry> data)
 	{
 	    super(context, rsrcId, txtId, data);
-	    this.context= context;
+	    this.mContext= context;
 	    this.items = data;
 	    this.viewResource = rsrcId;
 	}
@@ -40,16 +41,6 @@ public class RecentTalkListAdapter extends ArrayAdapter<RecentTalkEntry>
 	{
 		return items.size();
 	}
-	
-	public int getRealPosition(int pos)
-	{
-		if( pos < s_SEPRATE_POS )
-		{
-			return pos;
-		}else{
-			return pos-1;
-		}
-	}
 
 	@Override
     public View getView(int position, View convertView, ViewGroup parent)
@@ -58,35 +49,46 @@ public class RecentTalkListAdapter extends ArrayAdapter<RecentTalkEntry>
         
         if (v == null)
         {
-            LayoutInflater li = (LayoutInflater)context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            v = li.inflate(viewResource, null);
+            LayoutInflater li = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = li.inflate(viewResource, null); 
         } 
+   
         
-        RecentTalkEntry e = items.get(position); 
+        RecentTalkEntry e = items.get(position); 	// 각 포지션에 맞는 Entry를 가져온다.
         
-        if (e != null)
+        
+        if (e == null)	// null이면 구분자를 넣어준다. (임시)
         {
-        	ImageView IMG_label=(ImageView) v.findViewById(R.id.eRecentTalk_IMGLabel);
+        	ImageView img= new ImageView(mContext);
+        	img.setScaleType(ScaleType.FIT_XY);
+        	img.setImageResource(R.drawable.talk_list_now_label);
+        	//img.setLayoutParams(new LayoutParams(LayoutParams.FILL_PARENT, LayoutParams.WRAP_CONTENT));
+        	v=img;
+        }
+        else
+        {
+        	LayoutInflater li = (LayoutInflater)mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            v = li.inflate(viewResource, null); 
+            
+        	//ImageView IMG_label=(ImageView) v.findViewById(R.id.eRecentTalk_IMGLabel);
         	ImageView IMG_profile=(ImageView) v.findViewById(R.id.ePhoto);
         	TextView TXT_recentCount= (TextView) v.findViewById(R.id.eRecentTalkCount);        	
         	TextView TXT_Name= (TextView) v.findViewById(R.id.eRecentTalkName);
         	TextView TXT_Time= (TextView) v.findViewById(R.id.eRecentTalkTime);
         	TextView TXT_Content= (TextView) v.findViewById(R.id.eRecentTalkContent);
         	
-        	Calendar rightNow = Calendar.getInstance();
-        	int dayOFyear= rightNow.get(Calendar.DATE);
-        	int dayOFyear_talk = 0;
         	
-        	SimpleDateFormat format= new SimpleDateFormat("yyyy.MM.dd a h:mm");
         	try
         	{
+        		Calendar rightNow = Calendar.getInstance();        	
+            	SimpleDateFormat format= new SimpleDateFormat("yyyy.MM.dd a h:mm");
 				Date talkTime= format.parse(e.getTime());
 				
-	        	if( talkTime.getDate() == dayOFyear ) // 대화 날짜가 오늘이면
+	        	if( talkTime.getDate() == rightNow.get(Calendar.DATE) ) // 대화 날짜가 오늘이면
 	        	{
-	        		TXT_Time.setText( talkTime.getMonth()+"월 "+talkTime.getDate()+"일");
-	        	}else{
 	        		TXT_Time.setText( talkTime.getHours()+":"+talkTime.getMinutes() );
+	        	}else{
+	        		TXT_Time.setText( talkTime.getMonth()+"월 "+talkTime.getDate()+"일");
 	        	}
 			}
         	catch (ParseException e1)
@@ -111,10 +113,12 @@ public class RecentTalkListAdapter extends ArrayAdapter<RecentTalkEntry>
         	if( e.isEndChat() )
         	{
         		TXT_recentCount.setVisibility(View.INVISIBLE);
-        		IMG_label.setImageResource(R.drawable.talk_list_last_label);
-        	}else{
-        		IMG_label.setImageResource(R.drawable.talk_list_now_label);
+        		//IMG_label.setImageResource(R.drawable.talk_list_last_label);
         	}
+//        	else
+//        	{
+//        		IMG_label.setImageResource(R.drawable.talk_list_now_label);
+//        	}
             
         }
         
