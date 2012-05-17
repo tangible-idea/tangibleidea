f67ac469c7b1a276e70a68f551317ca361802840
thks10 <thks10@cb0aef14-ba3f-4046-a42c-e7904d01ad88>
2012-05-17 14:35:05 +0000

    -세팅>>내 프로필 변경 부분 레이아웃 세팅 (java, xml) -navigation bar와 setting_label 이미지를 나인패치로 변경

---
+++ trunk/meeple_android/src/com/tangibleidea/meeple/activity/ModifyProfileActivity.java
@@ -0,0 +1,22 @@
+package com.tangibleidea.meeple.activity;
+
+import com.tangibleidea.meeple.R;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ModifyProfileActivity extends Activity
+{
+
+	/* (non-Javadoc)
+	 * @see android.app.Activity#onCreate(android.os.Bundle)
+	 */
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.modify_myprofile);
+	}
+
+}
